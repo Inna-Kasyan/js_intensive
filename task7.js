c595eb7ea9836725647952fe7abdfbbb3d9c663b
Inna Kasyan <777inness@mail.ru>
2022-11-21 10:45:20 +0300

    task7

---
+++ task7.js
@@ -0,0 +1,7 @@
+/* Your task is to write a function maskify, which changes all but the last four characters into '#'.
+
+*/
+function maskify(cc) {
+  if (cc.length < 4) return cc;
+else return '#'.repeat(cc.length-4) + cc.slice(cc.length-4)
+}
